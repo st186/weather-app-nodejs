38afa035a5464aa2d0f6a7bbf78905b657c7446d
pat-011 <subham.tiwari186@gmail.com>
2019-07-06 17:27:36 +0530

    Remove unnessesary console.log calls

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-console.log('Client side Javascript loaded')
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
